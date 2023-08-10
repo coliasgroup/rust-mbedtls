aac9dab679ce875b662373750c27711ce9733c76
Nick Spinale <nick@nickspinale.com>
2023-08-10 20:46:55 +0000

    rand

--- mbedtls-platform-support/src/self_test.rs
+++ mbedtls-platform-support/src/self_test.rs
@@ -47,7 +47,7 @@ cfg_if::cfg_if! {
 
         // needs to be pub for global visiblity
         #[doc(hidden)]
-        #[no_mangle]
+        // #[no_mangle]
         pub unsafe extern "C" fn rand() -> c_int {
             rand_f.expect("Called self-test rand without enabling self-test")()
         }
